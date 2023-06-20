de59cf35f082140a3f7bad5eb089a51d713a31a6
Sameer kumar <2006Sameer2009@gmail.com>
2023-06-20 16:44:18 +0530

    firebase file edited

--- admin/src/firebase.js
+++ admin/src/firebase.js
@@ -2,14 +2,17 @@ import firebase from "firebase";
 
 const firebaseConfig = {
   apiKey: process.env.APP_KEY,
-  authDomain: "netflix-151df.firebaseapp.com",
-  projectId: "netflix-151df",
-  storageBucket: "netflix-151df.appspot.com",
-  messagingSenderId: "316700975498",
-  appId: "1:316700975498:web:15063159b205c1a349a873",
-  measurementId: "G-2ZGE63ES9F",
+  authDomain: "mern-netflix-clone-ba7de.firebaseapp.com",
+  projectId: "mern-netflix-clone-ba7de",
+  storageBucket: "mern-netflix-clone-ba7de.appspot.com",
+  messagingSenderId: "328437739861",
+  appId: "1:328437739861:web:5b94832637fc525c90ffdc",
+  measurementId: "G-4J3VN7DQWB"
 };
 
 firebase.initializeApp(firebaseConfig);
 const storage = firebase.storage();
 export default storage;
+
+
+
