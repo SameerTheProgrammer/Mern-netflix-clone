5fe983b83000b2c16e26dd48c0e2443f10974dbb
Sameer Kumar <134157712+SameerTheProgrammer@users.noreply.github.com>
2024-08-14 18:07:29 +0530

    Update Login.jsx

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,13 +1,15 @@
 import { useContext, useState } from "react";
 import { login } from "../../authContext/apiCalls";
 import { AuthContext } from "../../authContext/AuthContext";
+import { useHistory } from "react-router-dom";
 import "./login.scss";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { dispatch } = useContext(AuthContext);
-
+  const history = useHistory();
+  
   const handleLogin = (e) => {
     e.preventDefault();
     login({ email, password }, dispatch);
@@ -40,11 +42,14 @@ export default function Login() {
             Sign In
           </button>
           <span>
-            New to Netflix? <b>Sign up now.</b>
+            New to Netflix?
+            <b onClick={() => history.push("/register")}> Sign up</b>
           </span>
           <small>
-            This page is protected by Google reCAPTCHA to ensure you're not a
-            bot. <b>Learn more</b>.
+            <p>
+              Wants to login quickly with admin power use below data
+              <p>Email:- admin100@gmail.com Password:- 123456789</p>
+            </p>
           </small>
         </form>
       </div>
