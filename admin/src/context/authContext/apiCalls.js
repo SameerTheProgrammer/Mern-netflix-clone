23409a49427da1aaab52a2871ca43069d223a4e7
Sameer Kumar <134157712+SameerTheProgrammer@users.noreply.github.com>
2024-08-14 21:14:24 +0530

    Update apiCalls.js

--- admin/src/context/authContext/apiCalls.js
+++ admin/src/context/authContext/apiCalls.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 import { loginFailure, loginStart, loginSuccess } from "./AuthActions";
+import { apiUrl } from "../../constant"
 
 export const login = async (user, dispatch) => {
   dispatch(loginStart());
   try {
-    const res = await axios.post(`${process.env.BACKEND_URL}/auth/login`, user);
+    const res = await axios.post(`${apiUrl}/auth/login`, user);
     res.data.isAdmin && dispatch(loginSuccess(res.data));
   } catch (err) {
     dispatch(loginFailure());
