8649ddbcea53aae080e079b1c5830f7fe100f683
Sameer Kumar <134157712+SameerTheProgrammer@users.noreply.github.com>
2024-08-14 22:32:26 +0530

    Update Home.jsx

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -6,6 +6,7 @@ import WidgetSm from "../../components/widgetSm/WidgetSm";
 import WidgetLg from "../../components/widgetLg/WidgetLg";
 import { useEffect, useMemo, useState } from "react";
 import axios from "axios";
+import {apiUrl} from "../../constant";
 
 export default function Home() {
   const MONTHS = useMemo(
@@ -31,16 +32,16 @@ export default function Home() {
   useEffect(() => {
     const getStats = async () => {
       try {
-        const res = await axios.get(`${process.env.BACKEND_URL}/users/stats`, {
+        const res = await axios.get(`${apiUrl}/users/stats`, {
           headers: {
             token:
               "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwZTZmYzQ2NDk0Mjc3MTYwNDg4MmMxNiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYyNTgzMjMxMSwiZXhwIjoxNjI2MjY0MzExfQ.ATXV-1TTWIGyVBttTQSf0erRWjsgZ8jHQv1ZsUixbng",
           },
         });
-        const statsList = res.data.sort(function (a, b) {
+        const statsList = res.data?.sort(function (a, b) {
           return a._id - b._id;
         });
-        statsList.map((item) =>
+        statsList?.map((item) =>
           setUserStats((prev) => [
             ...prev,
             { name: MONTHS[item._id - 1], "New User": item.total },
