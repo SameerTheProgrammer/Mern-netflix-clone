30029a8bb30c4594aeb19e76a0513c1ae7167d23
Sameer Kumar <134157712+SameerTheProgrammer@users.noreply.github.com>
2024-08-15 08:58:19 +0530

    Update ListItem.jsx

--- client/src/components/listItem/ListItem.jsx
+++ client/src/components/listItem/ListItem.jsx
@@ -7,19 +7,19 @@ import {
 } from "@material-ui/icons";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { apiUrl } from "../../constant";
 import { Link } from "react-router-dom";
 
 export default function ListItem({ index, item }) {
   const [isHovered, setIsHovered] = useState(false);
-  const [movie, setMovie] = useState({});
+  const [movie, setMovie] = useState(null);
 
   useEffect(() => {
     const getMovie = async () => {
       try {
-        const res = await axios.get(`${process.env.BACKEND_URL}/movies/find/` + item, {
+        const res = await axios.get(`${apiUrl}/movies/find/` + item, {
           headers: {
-            token:
-            "Bearer "+JSON.parse(localStorage.getItem("user")).accessToken,
+            token: "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken,
           },
         });
         setMovie(res.data);
@@ -30,18 +30,26 @@ export default function ListItem({ index, item }) {
     getMovie();
   }, [item]);
 
+  if (!movie) {
+    return <div>No Movie Content</div>;
+  }
+
+  const leftPosition = isHovered ? index * 225 - 50 + index * 2.5 : 0;
+
   return (
     <Link to={{ pathname: "/watch", movie: movie }}>
       <div
         className="listItem"
-        style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
+        style={{ left: `${leftPosition}px` }}
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
       >
-        <img src={movie?.imgSm} alt="" />
+        <img src={movie?.imgSm} alt={movie?.title || "Movie Thumbnail"} />
         {isHovered && (
           <>
-            <video src={movie.trailer} autoPlay={true} loop />
+            {movie.trailer && (
+              <video src={movie.trailer} autoPlay={true} loop />
+            )}
             <div className="itemInfo">
               <div className="icons">
                 <PlayArrow className="icon" />
